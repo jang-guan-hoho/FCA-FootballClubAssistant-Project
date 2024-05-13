c2dedc0449c1af30349e9888f8a9536e86af11d5
NONE <NONE>
2024-05-13 14:58:43 +0900

    edit ERD img:

--- ERD(by ERDcloud)/FCA.sql
+++ ERD(by ERDcloud)/FCA.sql
@@ -1,120 +1,109 @@
-﻿CREATE TABLE `club` (
-	`id`	int	NOT NULL	COMMENT 'incresement',
-	`name`	varchar(40)	NOT NULL,
-	`max_member`	int	NOT NULL	COMMENT 'maximum 100',
-	`content`	text	NOT NULL	COMMENT 'placehold',
-	`fee`	int	NOT NULL	COMMENT '1000~',
-	`deadline`	int	NOT NULL	COMMENT '1~28',
-	`club_img`	varchar(40)	NULL	DEFAULT default_img,
-	`logo`	varchar(40)	NULL	DEFAULT default_logo,
-	`account`	int	NOT NULL,
-	`bank`	varchar(20)	NOT NULL,
-	`region`	varchar(40)	NOT NULL
+CREATE SCHEMA IF NOT EXISTS FCA;
+USE FCA;
+
+
+CREATE TABLE `club` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(40) NOT NULL,
+	`max_member` INT NOT NULL COMMENT 'maximum 100',
+	`content` TEXT NOT NULL COMMENT 'placehold',
+	`fee` INT NOT NULL COMMENT '1000~',
+	`deadline` INT NOT NULL COMMENT '1~28',
+	`club_img` VARCHAR(40) NULL DEFAULT 'default_img',
+	`logo` VARCHAR(40) NULL DEFAULT 'default_logo',
+	`account` INT NOT NULL,
+	`bank` VARCHAR(20) NOT NULL,
+	`region` VARCHAR(40) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+CREATE TABLE `user` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`email` VARCHAR(40) NOT NULL,
+	`password` VARCHAR(20) NOT NULL,
+	`name` VARCHAR(20) NOT NULL,
+	`gender` ENUM('M', 'F') NOT NULL COMMENT '남 - M / 여 - F',
+	`birth` DATE NOT NULL,
+	`intro` TEXT NOT NULL COMMENT 'placehold',
+	`profile` VARCHAR(40) NULL,
+	PRIMARY KEY (`id`)
+);
+CREATE TABLE `place` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(40) NOT NULL,
+	`address` VARCHAR(100) NOT NULL,
+	`url` VARCHAR(100) NULL,
+	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `schedule` (
-	`id`	int	NOT NULL	COMMENT 'incresement',
-	`date`	timestamp	NOT NULL,
-	`match`	int	NULL,
-	`equipment`	varchar(40)	NULL,
-	`p_id`	int	NOT NULL	COMMENT 'incresement',
-	`c_id`	int	NOT NULL	COMMENT 'incresement'
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`date` TIMESTAMP NOT NULL,
+	`match` INT NULL,
+	`equipment` VARCHAR(40) NULL,
+	`p_id` INT NOT NULL,
+	`c_id` INT NOT NULL,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`c_id`) REFERENCES `club`(`id`),
+    FOREIGN KEY (`p_id`) REFERENCES `place`(`id`)
 );
 
-CREATE TABLE `user` (
-	`id`	int	NOT NULL	COMMENT 'incresement',
-	`email`	varchar(40)	NOT NULL,
-	`password`	varchar(20)	NOT NULL,
-	`name`	varchar(20)	NOT NULL,
-	`gender`	boolean	NOT NULL	COMMENT '남 - T / 여 - F',
-	`birth`	date	NOT NULL,
-	`intro`	text	NOT NULL	COMMENT 'placehold',
-	`profile`	varchar(40)	NULL
-);
+
 
 CREATE TABLE `club_member` (
-	`id`	int	NOT NULL	COMMENT 'incresement',
-	`c_id`	int	NOT NULL	COMMENT 'incresement',
-	`u_id`	int	NOT NULL	COMMENT 'incresement',
-	`position`	varchar(20)	NOT NULL,
-	`Field`	VARCHAR(255)	NOT NULL
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`c_id` INT NOT NULL,
+	`u_id` INT NOT NULL,
+	`position` VARCHAR(20) NOT NULL,
+	`billing_key` VARCHAR(255) NOT NULL,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`c_id`) REFERENCES `club`(`id`),
+	FOREIGN KEY (`u_id`) REFERENCES `user`(`id`)
 );
 
 CREATE TABLE `participant` (
-	`id`	int	NOT NULL	COMMENT 'inscresement',
-	`s_id`	int	NOT NULL	COMMENT 'incresement',
-	`u_id`	int	NOT NULL	COMMENT 'incresement'
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`s_id` INT NOT NULL,
+	`u_id` INT NOT NULL,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`s_id`) REFERENCES `schedule`(`id`),
+	FOREIGN KEY (`u_id`) REFERENCES `user`(`id`)
 );
 
-CREATE TABLE `place` (
-	`id`	int	NOT NULL	COMMENT 'incresement',
-	`name`	varchar(40)	NOT NULL,
-	`address`	varchar(100)	NOT NULL,
-	`url`	varchar(100)	NULL
-);
 
 CREATE TABLE `receipt` (
-	`id`	int	NOT NULL	COMMENT 'incresement',
-	`item`	varchar(40)	NOT NULL,
-	`price`	int	NOT NULL,
-	`s_id`	int	NOT NULL	COMMENT 'incresement'
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`item` VARCHAR(40) NOT NULL,
+	`price` INT NOT NULL,
+	`s_id` INT NOT NULL,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`s_id`) REFERENCES `schedule`(`id`)
 );
 
 CREATE TABLE `payment` (
-	`id`	int	NOT NULL	COMMENT 'incresement',
-	`pay_date`	date	NOT NULL,
-	`status`	boolean	NOT NULL,
-	`comment`	varchar(200)	NOT NULL,
-	`c_id`	int	NOT NULL	COMMENT 'incresement',
-	`u_id`	int	NOT NULL	COMMENT 'incresement'
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`pay_date` DATE NOT NULL,
+	`status` BOOLEAN NOT NULL,
+	`comment` VARCHAR(200) NOT NULL,
+	`c_id` INT NOT NULL,
+	`u_id` INT NOT NULL,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`c_id`) REFERENCES `club`(`id`),
+	FOREIGN KEY (`u_id`) REFERENCES `user`(`id`)
 );
 
 CREATE TABLE `board` (
-	`id`	int	NOT NULL	COMMENT 'incresement',
-	`title`	varchar(40)	NOT NULL	COMMENT 'incresement',
-	`content`	text	NOT NULL	COMMENT 'placehold',
-	`category`	boolean	NOT NULL	COMMENT '공지 - T / 일반 - F',
-	`date`	date	NOT NULL,
-	`u_id`	int	NOT NULL	COMMENT 'incresement',
-	`c_id`	int	NOT NULL	COMMENT 'incresement',
-	`viewCnt`	int	NULL	DEFAULT 0,
-	`img`	varchar(40)	NULL,
-	`video`	varchar(40)	NULL
-);
-
-ALTER TABLE `club` ADD CONSTRAINT `PK_CLUB` PRIMARY KEY (
-	`id`
-);
-
-ALTER TABLE `schedule` ADD CONSTRAINT `PK_SCHEDULE` PRIMARY KEY (
-	`id`
-);
-
-ALTER TABLE `user` ADD CONSTRAINT `PK_USER` PRIMARY KEY (
-	`id`
-);
-
-ALTER TABLE `club_member` ADD CONSTRAINT `PK_CLUB_MEMBER` PRIMARY KEY (
-	`id`
-);
-
-ALTER TABLE `participant` ADD CONSTRAINT `PK_PARTICIPANT` PRIMARY KEY (
-	`id`
-);
-
-ALTER TABLE `place` ADD CONSTRAINT `PK_PLACE` PRIMARY KEY (
-	`id`
-);
-
-ALTER TABLE `receipt` ADD CONSTRAINT `PK_RECEIPT` PRIMARY KEY (
-	`id`
-);
-
-ALTER TABLE `payment` ADD CONSTRAINT `PK_PAYMENT` PRIMARY KEY (
-	`id`
-);
-
-ALTER TABLE `board` ADD CONSTRAINT `PK_BOARD` PRIMARY KEY (
-	`id`
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`title` VARCHAR(40) NOT NULL,
+	`content` TEXT NOT NULL COMMENT 'placehold',
+	`category` ENUM('Notice', 'General') NOT NULL COMMENT '공지 - Notice / 일반 - General',
+	`date` DATE NOT NULL,
+	`u_id` INT NOT NULL,
+	`c_id` INT NOT NULL,
+	`viewCnt` INT NULL DEFAULT 0,
+	`img` VARCHAR(40) NULL,
+	`video` VARCHAR(40) NULL,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`u_id`) REFERENCES `user`(`id`),
+	FOREIGN KEY (`c_id`) REFERENCES `club`(`id`)
 );
 
